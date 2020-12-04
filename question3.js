edeaec3f0d8a0b27ce3b7571922364b01ef1d7f9
Jyoti Bista <zyotieebista12@gmail.com>
2020-12-04 20:30:55 +0545

    first commit

---
+++ question3.js
@@ -0,0 +1,5 @@
+var fav_place = ["Biratnagar", "Lalitpur", "Patan", "Kathmandu", "Pokhara", "Palpa"];
+fav_place.push("Lumbini", "Janakpur");
+fav_place.splice(3, 1);
+console.log("New array is " + fav_place);
+console.log("Length of array is " + fav_place.length);
